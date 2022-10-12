011b6ad1f651c98b173861c77ce09d79cac16f38
Anuragkumarsah <asah2307@gmail.com>
2022-10-12 17:49:00 +0530

    added web scrapper

--- scrapper.js
+++ scrapper.js
@@ -5,14 +5,7 @@ export const scrapeList = async (url) => {
   const page = await browser.newPage();
   await page.goto(url);
   const elements = await page.$$(".filler > .Episodes");
-  //   elements.map((ele) => {
-  //     console.log(ele.jsonValue());
-  //   });
-  //   let ep = await page.evaluate(
-  //     () => document.querySelector(".Episodes").innerText
-  //   );
   let ep = await (await elements[0].getProperty("textContent")).jsonValue();
-  // let ep = await elements.getProperty("textContent").jsonValue();
   browser.close();
   return { filler: ep };
 };
